c2d0d30b18e2b8acce35cbe297569414e67f6187
Silas Wolff <siwo@itu.dk>
2024-02-08 02:54:56 +0100

    exercises 2.1 - 2.4

--- Assignment2/Assignment2.fs
+++ Assignment2/Assignment2.fs
@@ -1,17 +1,46 @@
 module Assignment2
 
-    let downto1 _ = failwith "not implemented"
+    //Exercise 2.1 Write a function downto1 : int -> int list that given an integer n returns the n-element list [n; n-1; ...; 1] if
+    //n > 0 and [] otherwise. You must use if-then-else expressions to define the function.
+    let downto1 (n:int) =
+        if n = 0 then []
+        else [n .. -1 .. 1]
     
-    let downto2 _ = failwith "not implemented"
+    //Secondly define the function downto2 having same semantics as downto1. This time you must use pattern matching.
+    let downto2 (n:int) =
+        match n with
+        | 0 -> []
+        | _ -> [n .. -1 .. 1]
 
-    let removeOddIdx _ = failwith "not implemented"
+    //Exercise 2.2 - Write a function removeOddIdx : 'a list -> 'a list that given a list xs returns a list where all odd-indexed
+    //elements of xs have been removed.
+    let rec removeOddIdx xs =
+        match xs with
+        | [] -> []
+        | [x] -> [x]
+        | y :: _ :: ys -> y :: removeOddIdx ys
 
-    let combinePair _ = failwith "not implemented"
+    // Exercise 2.3 - Write a function combinePair : 'a list -> ('a * 'a) list that given a list xs returns the list with elements from xs
+    // combined into pairs. If xs contains an odd number of elements, then the last element is thrown away.
+    let rec combinePair xs =
+        match xs with
+        | [] -> []
+        | [_] -> []
+        | x :: y :: ys -> (x, y) :: combinePair ys
 
-
-    type complex = unit // Fill in your type here
-    let mkComplex _ = failwith "not implemented"
-    let complexToPair _ = failwith "not implemented"
+    // Exercise 2.4 - The set of complex numbers is the set of pairs of real numbers.
+    // Define a type 'complex' that represents complex numbers with floating point components.
+    type complex = float * float
+    
+    //Define a function mkComplex : float -> float -> complex that given two floating point numbers return the corresponding complex number.
+    let mkComplex (a:float) (b:float) : complex =
+        (a, b)
+    
+    //Define a function complexToPair : complex -> float * float that given a complex number (a,b) returns the pair (a, b).
+    let complexToPair ((a,b) : complex) : float * float =
+        (a,b)
+        
+    
     let (|+|) _ = failwith "not implemented"
     let (|*|) _ = failwith "not implemented"
     let (|-|) _ = failwith "not implemented"
